4177a9eaa5b4421a9666aff126c1394c2292808a
lamyaIs <44745572+lamyaIs@users.noreply.github.com>
2024-11-12 13:19:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if (!defined('_PS_VERSION_')) {
 }
 
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
