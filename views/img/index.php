28c8492306dc225d54ea48bf4252d1608decef02
lamyaIs <44745572+lamyaIs@users.noreply.github.com>
2024-11-12 10:54:21 +0100

    Update index.php

--- views/img/index.php
+++ views/img/index.php
@@ -1,29 +1,29 @@
-<?php
-/**
- * Copyright Mr-dev
- *
- * NOTICE OF LICENSE
- *
- * This file is proprietary and each license is valid for use on one website only.
- * To use this file on additional websites or projects, additional licenses must be purchased.
- * Redistribution, reselling, leasing, licensing, sub-licensing, or offering this resource to any third party is strictly prohibited.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to keep compatibility with future PrestaShop updates.
- *
- * @author     Mr-dev
- * @copyright  Mr-dev
- * @license    License valid for one website (or project) per purchase
- */
- 
-if (!defined('_PS_VERSION_')) {
-    exit;
-}
-header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
-header('Cache-Control: no-store, no-cache, must-revalidate');
-header('Cache-Control: post-check=0, pre-check=0', false);
-header('Pragma: no-cache');
-header('Location: ../');
-exit;
+<?php
+/**
+ * Copyright Mr-dev
+ *
+ * NOTICE OF LICENSE
+ *
+ * This file is proprietary and each license is valid for use on one website only.
+ * To use this file on additional websites or projects, additional licenses must be purchased.
+ * Redistribution, reselling, leasing, licensing, sub-licensing, or offering this resource to any third party is strictly prohibited.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to keep compatibility with future PrestaShop updates.
+ *
+ * @author     Mr-dev
+ * @copyright  Mr-dev
+ * @license    License valid for one website (or project) per purchase
+ */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+header('Location: ../');
+exit;
