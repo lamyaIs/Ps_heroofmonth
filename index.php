e4c2b6f1893a6b6624e7e89a38c5bba80c300ecf
lamyaIs <44745572+lamyaIs@users.noreply.github.com>
2024-11-12 10:36:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright Mr-dev
  *
@@ -19,6 +20,7 @@
 if (!defined('_PS_VERSION_')) {
     exit;
 }
+
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
 header('Cache-Control: no-store, no-cache, must-revalidate');
