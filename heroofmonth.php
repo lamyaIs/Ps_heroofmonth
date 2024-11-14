d83d9f5aeecb66044c172a1d370db7fcb8e75920
lamyaIs <44745572+lamyaIs@users.noreply.github.com>
2024-11-14 11:22:04 +0100

    Update heroofmonth.php

--- heroofmonth.php
+++ heroofmonth.php
@@ -26,7 +26,7 @@ class HeroOfMonth extends Module
     {
         $this->name = 'heroofmonth';
         $this->tab = 'front_office_features';
-        $this->module_key = 'c1614c239af92968e5fae97f366e9961';
+        $this->module_key = '1e21658d893412fdc659137f2147bfe4';
         $this->version = '1.1.9';
         $this->author = 'Mr-dev';
         $this->need_instance = 0;
