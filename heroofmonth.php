bee225bd99c42fcfb0ed3d04c56327897d02a3c7
lamyaIs <44745572+lamyaIs@users.noreply.github.com>
2024-11-15 23:03:31 +0100

    Update heroofmonth.php

--- heroofmonth.php
+++ heroofmonth.php
@@ -184,7 +184,7 @@ class HeroOfMonth extends Module
         Media::addJsDef([
             'ajaxUrl' => $this->context->link->getAdminLink('AdminModules', true) . ' & configure=' . $this->name . ' & ajax=1 & action=searchProduct',
         ]);
-        $output .= '<a href="' . $this->context->link->getAdminLink('AdminModules', false) . '&configure=' . $this->name . '&action=layoutSettings&token=' . Tools::getAdminTokenLite('AdminModules') . '" class="btn btn-primary" style="margin-bottom: 20px;">' . $this->l('Configuration du produit du mois') . '</a>';
+        $output .= '<a href="' . $this->context->link->getAdminLink('AdminModules', false) . '&configure=' . $this->name . '&action=layoutSettings&token=' . Tools::getAdminTokenLite('AdminModules') . '" class="btn btn-primary" style="margin-bottom: 20px;">' . $this->l('Product of the Month Configuration') . '</a>';
         if ('layoutSettings' === Tools::getValue('action')) {
             return $this->renderLayoutSettingsForm();
         }
